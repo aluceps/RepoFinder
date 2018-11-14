a731ddd79425fdcbc3f03299ee9b952f4f435f8f
aluceps <aluceps@gmail.com>
2018-11-14 17:43:07 +0900

    reformat for databinding.

--- app/src/main/java/me/aluceps/repofinder/MainActivity.kt
+++ app/src/main/java/me/aluceps/repofinder/MainActivity.kt
@@ -1,12 +1,14 @@
 package me.aluceps.repofinder
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.aluceps.repofinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
