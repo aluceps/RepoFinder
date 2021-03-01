d95b6131921cda9fd7eb8e57b3ca906fc22d3876
aluceps <aluceps@gmail.com>
2021-03-01 18:53:19 +0900

    plugins version move to Deps.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,9 +26,8 @@ android {
 }
 
 dependencies {
-    val kotlin_version: String by rootProject.extra
     fileTree("dir" to "libs", "include" to listOf("*.jar"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Deps.Kotlin.version}")
 
     // support libraries
     implementation("androidx.appcompat:appcompat:1.0.2")
@@ -39,11 +38,11 @@ dependencies {
     implementation("com.google.android.material:material:1.1.0-alpha09")
 
     // DI
-    implementation("com.google.dagger:dagger:2.23.1")
-    implementation("com.google.dagger:dagger-android:2.23.1")
-    implementation("com.google.dagger:dagger-android-support:2.23.1")
-    kapt("com.google.dagger:dagger-compiler:2.23.1")
-    kapt("com.google.dagger:dagger-android-processor:2.23.1")
+    implementation("com.google.dagger:dagger:${Deps.Dagger.version}")
+    implementation("com.google.dagger:dagger-android:${Deps.Dagger.version}")
+    implementation("com.google.dagger:dagger-android-support:${Deps.Dagger.version}")
+    kapt("com.google.dagger:dagger-compiler:${Deps.Dagger.version}")
+    kapt("com.google.dagger:dagger-android-processor:${Deps.Dagger.version}")
 
     // Rx
     implementation("io.reactivex.rxjava2:rxjava:2.2.6")
@@ -51,11 +50,11 @@ dependencies {
     implementation("io.reactivex.rxjava2:rxkotlin:2.3.0")
 
     // Http Client
-    implementation("com.squareup.retrofit2:retrofit:2.6.1")
-    implementation("com.squareup.retrofit2:converter-gson:2.4.0")
-    implementation("com.squareup.retrofit2:adapter-rxjava2:2.6.1")
-    implementation("com.squareup.okhttp3:okhttp:4.1.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.1.0")
+    implementation("com.squareup.retrofit2:retrofit:${Deps.Retrofit.version}")
+    implementation("com.squareup.retrofit2:converter-gson:${Deps.Retrofit.version}")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:${Deps.Retrofit.version}")
+    implementation("com.squareup.okhttp3:okhttp:${Deps.OkHttp.version}")
+    implementation("com.squareup.okhttp3:logging-interceptor:${Deps.OkHttp.version}")
     implementation("com.google.code.gson:gson:2.8.5")
 
     // Image Loader
